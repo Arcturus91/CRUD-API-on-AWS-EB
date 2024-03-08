916b5abdd96d1d821dab5a016a0f767297aa8fe7
arturo-zarzilla <arturo-zarzilla@io2mail.com>
2024-03-07 19:56:45 -0500

    implement all routes

--- routes/product.js
+++ routes/product.js
@@ -1,26 +1,32 @@
-const express = require("express");
-
+const express = require('express');
 const router = express.Router();
-const AWS = require("aws-sdk");
-
+const AWS = require('aws-sdk');
 AWS.config.update({
   region: "sa-east-1",
 });
-
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
-const dynamoDBTableName = "product-inventory";
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dynamodbTableName = 'product-inventory';
 
 //reference https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html
 //get single item
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const params = {
-    TableName: dynamoDBTableName,
+    TableName: dynamodbTableName,
     Key: {
-      productId: req.query.productId,
-    },
-  };
+      'productId': req.query.productId
+    }
+  }
+  await dynamodb.get(params).promise().then(response => {
+    res.json(response.Item);
+  }, error => {
+    console.error('Do your custom error handling here. I am just ganna log it out: ', error);
+    res.status(500).send(error);
+  })
+})
 
-  /* var request = s3.putObject({Bucket: 'bucket', Key: 'key'});
+router.get('/all', async (req, res) => {
+
+   /* var request = s3.putObject({Bucket: 'bucket', Key: 'key'});
         var result = request.promise();
         result.then(function(data) { ... }, function(error) { ... });
 
@@ -30,72 +36,96 @@ Two callbacks can be provided to the then method on the returned promise.
 The first callback will be called if the promise is fulfilled, 
 and the second callback will be called if the promise is rejected.
     */
-
-  await dynamoDB.get(params).promise.then(
-    (response) => {
-      res.json(response.Item);
-    },
-    (error) => {
-      console.error("Error while getting item ", error);
-      res.status(500).send(error);
-    }
-  );
-});
-
-router.get("/all", async (req, res) => {
+   
   const params = {
-    TableName: dynamoDBTableName,
-  };
-
+    TableName: dynamodbTableName
+  }
   try {
     const allProducts = await scanDynamoRecords(params, []);
     const body = {
-      products: allProducts,
-    };
+      products: allProducts
+    }
     res.json(body);
-  } catch (error) {
-    console.error("Error while getting all items ", error);
+  } catch(error) {
+    console.error('Do your custom error handling here. I am just ganna log it out: ', error);
     res.status(500).send(error);
   }
-});
-router.post("/", async (req, res) => {
-  const params = {
-    TableName: dynamoDBTableName,
-    Item: req.body,
-  };
-
-  await dynamoDB.put(params).promise.then(
-    () => {
-      const body = {
-        Operation: "SAVE",
-        Message: "SUCCESS",
-        Item: req.body,
-      };
-      res.json(body);
-    },
-    (error) => {
-      console.error("Error while getting item ", error);
-      res.status(500).send(error);
-    }
-  );
-});
-/* router.path('/',async (req,res)=>{
-
 })
-router.delete('/',async (req,res)=>{
 
-}) */
+router.post('/', async (req, res) => {
+  const params = {
+    TableName: dynamodbTableName,
+    Item: req.body
+  }
+  await dynamodb.put(params).promise().then(() => {
+    const body = {
+      Operation: 'SAVE',
+      Message: 'SUCCESS',
+      Item: req.body
+    }
+    res.json(body);
+  }, error => {
+    console.error('Do your custom error handling here. I am just ganna log it out: ', error);
+    res.status(500).send(error);
+  })
+})
+
+router.patch('/', async (req, res) => {
+  const params = {
+    TableName: dynamodbTableName,
+    Key: {
+      'productId': req.body.productId
+    },
+    UpdateExpression: `set ${req.body.updateKey} = :value`,
+    ExpressionAttributeValues: {
+      ':value': req.body.updateValue
+    },
+    ReturnValues: 'UPDATED_NEW'
+  }
+  await dynamodb.update(params).promise().then(response => {
+    const body = {
+      Operation: 'UPDATE',
+      Message: 'SUCCESS',
+      UpdatedAttributes: response
+    }
+    res.json(body);
+  }, error => {
+    console.error('Do your custom error handling here. I am just ganna log it out: ', error);
+    res.status(500).send(error);
+  })
+})
+
+router.delete('/', async (req, res) => {
+  const params = {
+    TableName: dynamodbTableName,
+    Key: {
+      'productId': req.body.productId
+    },
+    ReturnValues: 'ALL_OLD'
+  }
+  await dynamodb.delete(params).promise().then(response => {
+    const body = {
+      Operation: 'DELETE',
+      Message: 'SUCCESS',
+      Item: response
+    }
+    res.json(body);
+  }, error => {
+    console.error('Do your custom error handling here. I am just ganna log it out: ', error);
+    res.status(500).send(error);
+  })
+})
 
 async function scanDynamoRecords(scanParams, itemArray) {
   try {
-    const dynamoData = await dynamoDB.scan(scanParams).promise();
+    const dynamoData = await dynamodb.scan(scanParams).promise();
     itemArray = itemArray.concat(dynamoData.Items);
     if (dynamoData.LastEvaluatedKey) {
       scanParams.ExclusiveStartKey = dynamoData.LastEvaluatedKey;
       return await scanDynamoRecords(scanParams, itemArray);
     }
     return itemArray;
-  } catch (error) {
+  } catch(error) {
     throw new Error(error);
   }
 }
